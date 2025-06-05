2ec3558be7a2f9b98424f75935c81e968eb7a848
davidmizra17 <davidmizrago@gmail.com>
2025-06-05 14:40:09 -0400

    feat: responding to multiple ping commands sent by the same connection

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     
@@ -13,9 +12,16 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buffer = [0; 512];
+                loop {
+                    let bytes_read = stream.read(&mut buffer).unwrap();
+                    if bytes_read == 0 {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwarap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
