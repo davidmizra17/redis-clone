bca98144fe4cca794cc8e6e105e7be027e730458
davidmizra17 <davidmizrago@gmail.com>
2025-06-13 15:20:01 -0400

    feat: array parsing

--- src/resp.rs
+++ src/resp.rs
@@ -82,6 +82,26 @@ fn parse_bulk_string(buffer: BytesMut) -> Result<(Value, usize)> {
     Ok((Value::BulkString(String::from_utf8(buffer[bytes_consumed..end_of_bulk_str].to_vec())?), total_parsed))
 }
 
+fn parse_array(buffer: BytesMut) -> Result<(Value, usize)> {
+    let (array_length, mut bytes_consumed) = if let Some((line, len)) = read_until_crlf(&buffer[1..]) {
+        let array_length = parse_int(line)?;
+
+        (array_length, len + 1)
+    } else {
+        return Err(anyhow::anyhow!("Invalid array format {:?}", buffer));
+    };
+
+    let mut items = vec![];
+    for _ in 0..array_length {
+        let (array_item, len) = parse_message(BytesMut::from(&buffer[bytes_consumed..]))?;
+
+        items.push(array_item);
+        bytes_consumed += len;
+    }
+
+    return Ok((Value::Array(items), bytes_consumed))
+}
+
 fn read_until_clrf(buffer: &[u8]) -> Option<(&[u8], usize)> {
     for i in 1..buffer.len() {
         if buffer[i - 1] == b'\r' && buffer[i] == b'\n' {
